b51df4ae87f8f69bb7ab4bd96444add9b5fa75d4
sirlocust <jua56yu@hotmail.com>
2020-08-20 05:57:03 -0500

    creacion de componentes adicionales

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent implements OnInit {
+
+  @Input() contador: number;
+  @Output() cambioContador = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  multiplicar(): void{
+    this.contador *= 2;
+    this.emitirContador();
+  }
+
+  dividir(): void{
+    this.contador /= 2;
+    this.emitirContador();
+  }
+
+  emitirContador(): void{
+    this.cambioContador.emit(this.contador);
+  }
+}
